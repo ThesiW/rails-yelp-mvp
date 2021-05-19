44204f63476854595c89a18f5237c3ea916b3c1f
Theresa Wendelstadt <theresa@wendelstadt.de>
2021-05-19 16:37:28 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+agrikultur = { name: 'Agrikultur', address: 'Stockholm', category: 'belgian' }
+riche = { name: 'Riche', address: 'Paris', category: 'french' }
+ai_ramen = { name: 'Ai Ramen', address: 'Berlin', category: 'japanese' }
+barra = { name: 'Barra', address: 'Rome', category: 'italian' }
+petrus = { name: 'Petrus', address: 'Shanghai', category: 'chinese' }
+
+[ agrikultur, riche, ai_ramen, barra, petrus ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
